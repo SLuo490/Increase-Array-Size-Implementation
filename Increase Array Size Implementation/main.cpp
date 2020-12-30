87fa76c500cafb9ff3d3cdf663ab11604bf1a26d
Shi Tao Luo <sluo5991@gmail.com>
2020-12-30 18:10:03 -0500

    increase array size implementation using dynamic array

--- Increase Array Size Implementation/main.cpp
+++ Increase Array Size Implementation/main.cpp
@@ -1,14 +1,36 @@
-//
-//  main.cpp
-//  Increase Array Size Implementation
-//
-//  Created by Shi Tao Luo on 12/30/20.
-//
+//increase array size using dynamic array
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main () {
+    //create a array of size 5
+    int *p;
+    p = new int[5];
+    
+    //add values to array p
+    for (int i = 0; i < 5; i++) {
+        p[i] = i;
+    }
+    
+    //create a new array of size 10;
+    int *q;
+    q = new int[10];
+    
+    //copy data from array p into another array q
+    for (int i = 0; i < 5; i++) {
+        q[i] = p[i];
+    }
+    
+    //delete array p
+    delete[]p;
+    p = q;
+    q = NULL;
+    
+    //show array p;
+    for (int i = 0; i < 5; i++) {
+        cout << p[i] << endl;
+    }
+    
     return 0;
 }
