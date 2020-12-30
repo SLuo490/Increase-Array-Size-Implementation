0b33a8a24c8aa0886c8b101d08deaa0fae048b5f
Shi Tao Luo <sluo5991@gmail.com>
2020-12-30 18:04:06 -0500

    Initial Commit

---
+++ Increase Array Size Implementation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Increase Array Size Implementation
+//
+//  Created by Shi Tao Luo on 12/30/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
